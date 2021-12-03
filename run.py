124380c553237c9d53c8ce2a5c6dfba5ff713393
Kat632 <katherine.biggs@mail.com>
2021-12-03 16:05:31 +0000

    Add descriptions to functions, clean up code, add more questions

--- run.py
+++ run.py
@@ -76,16 +76,20 @@ print('''\u001b[32m
 \u001b[0m''')
 
 print("Sul sul!  Welcome to the Sims 4 Real Estate Quiz...\n")
-print('''\u001b[32m===================================================\u001b[0m''')
-name = input("Please enter your name: ")
+print('''\u001b[32m===============================================\u001b[0m''')
+name = input("Please enter your name:\n ")
 time.sleep(1)
 clear_terminal()
-print('''\u001b[32m===================================================\u001b[0m''')
+print('''\u001b[32m===============================================\u001b[0m''')
 print("Thank you for stopping by, " + name)
-print('''\u001b[32m===================================================\u001b[0m''')
+print('''\u001b[32m===============================================\u001b[0m''')
 
 
 class Question:
+    """
+    Questions instance, gets the question
+    and the answer
+    """
     def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
@@ -148,7 +152,7 @@ question_prompts = [
      (c)Uptown\n ",
 
      "What is the name of the house where the Hecking family live \
-         in Brindleton Bay?\n \
+     in Brindleton Bay?\n \
      (a)Dogpaw Cottage\n \
      (b)Chateau Rosie\n \
      (c)It's A Good House\n ",
@@ -183,12 +187,10 @@ question_prompts = [
      (b)Catsclaw Cottage\n \
      (c)Whiskerman's Wharf\n",
 
-
-
-
-
-
-
+     "Where is Isle of Volpe National Park?\n \
+     (a)Sulani\n \
+     (b)Selvadorada\n \
+     (c)Henford-on-Bagley\n",
 
 ]
 
@@ -211,11 +213,17 @@ questions = [
     Question(question_prompts[15], "c"),
     Question(question_prompts[16], "b"),
     Question(question_prompts[17], "a"),
-
+    Question(question_prompts[18], "c"),
 ]
 
 
 def run_quiz(questions):
+    """
+    Run the quiz. Start the timer.
+    Randomise the questions.
+    Keep track of score.
+    Send score to leaderboard.
+    """
     tic = time.perf_counter()
     score = 0
     sampled_list = random.sample(questions, 10)
@@ -247,6 +255,31 @@ def run_quiz(questions):
         scores.sort((2, 'des'), (4, 'des'),)
         time.sleep(1)
         print("Leaderboard updated successfully!\n")
+        print('''\u001b[32m======================================\u001b[0m''')
+        print("Press 'A' to restart the game\n")
+        print("Press 'B' to view the leaderboard\n")
+        print("Or press any key to exit the game.")
+        print('''\u001b[32m======================================\u001b[0m''')
+        answer_final = input().lower()
+        if answer_final == "a":
+            time.sleep(1)
+            print("Veena frendishay.  Starting a new quiz...")
+            run_quiz(questions)
+        if answer_final == "b":
+            clear_terminal()
+            print(tabulate(data, headers='firstrow', tablefmt='fancy_grid'))
+            time.sleep(5)
+            print("Would you like to play the game now?\n")
+            answer = (input("Press Y to start the game or any key to exit\n"))
+            if answer == ("y"):
+                print("Veena fredishay! Starting a new quiz...")
+                time.sleep(1)
+                clear_terminal()
+                run_quiz(questions)
+        else:
+            print("Exiting the game...")
+            time.sleep(1)
+            game_over()
     else:
         time.sleep(1)
         print("Ok, thank you for playing, " + name)
